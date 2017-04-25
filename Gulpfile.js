440ef39dd11199e7f82d97c629d10c25159dd7d0
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-04-24 21:13:51 -0500

    modifies Gulpfile.js to watch scss directory and put new file in css directory. Also adds some scss stuff

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,1 +1,21 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const input = './scss/**/*.scss';
+
+gulp.task('default', ()=>{
+  gulp.watch('scss/**/*.scss', ['sass'])
+})
+
+gulp.task('watch', ()=>{
+  return gulp
+        .watch(input, ['sass'])
+        .on('change', (event)=>{
+          console.log('File ' + event.path + ' was ' + event.type + ', running tasks...')
+        })
+})
+
+gulp.task('sass', ()=>{
+  gulp.src('scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css/'))
+})
